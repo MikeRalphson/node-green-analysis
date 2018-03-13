5979193112bdb881ded78b6bab4648bce0ad8016
Mike Ralphson <mike.ralphson@gmail.com>
2018-03-13 10:36:04 +0000

    subheadings ok

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ let $ = cheerio.load(htmlStr);
 
 $('article').each(function(e){
     console.log($(this).children('header').first().children('h1').first().children('a').first().text());
-    let caption = $(this).children('table .results').first().children('caption').first();
-    console.log($(caption).text());
+    let tables = $(this).children('table');
+    $(tables).each(function(i,t){
+        let link = $(this).children('caption').first().children('h2').first().children('a').first();
+        console.log(' ',$(link).text());
+    });
 });
 
 
